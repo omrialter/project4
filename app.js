4075a461755fb1b54e2570f065878912b798ab48
omri <omrialter3@gmail.com>
2023-04-12 21:17:56 +0300

    cors

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ const express = require("express");
 const http = require("http");
 const path = require("path");
 const { routesInit } = require("./routes/configRoutes")
+const cors = require("cors");
 
 require("./db/mongoConnect")
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 
 app.use(express.static(path.join(__dirname, "public")));
