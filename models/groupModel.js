4380251df3f81ff503af2483ab2f8bca2eb1b9df
omri <omrialter3@gmail.com>
2023-06-15 17:14:26 +0300

    groups

--- models/groupModel.js
+++ models/groupModel.js
@@ -3,25 +3,28 @@ const Joi = require("joi");
 
 let schema = new mongoose.Schema({
     description: String,
-    name: String,
-    user_id: String,
-    coverPic: {
-        type: String, default: ""
+    group_name: String,
+    group_admin: String,
+    posts: {
+        type: Array, default: []
     },
     members: {
         type: Array, default: []
+    },
+    coverPic: {
+        type: String, default: ""
     },
     date_created: {
         type: Date, default: Date.now
     },
 
 })
-exports.groupModel = mongoose.model("groups", schema)
+exports.GroupModel = mongoose.model("groups", schema)
 
 exports.validateGroups = (_reqBody) => {
     let joiSchema = Joi.object({
         description: Joi.string().min(1).max(500).allow(null, ""),
-        name: Joi.string().min(2).max(40).required(),
+        group_name: Joi.string().min(2).max(40).required(),
     })
     return joiSchema.validate(_reqBody)
 }
--- routes/groups.js
+++ routes/groups.js
@@ -1,8 +1,28 @@
 const express = require("express");
+const { validateGroups, GroupModel } = require("../models/groupModel");
 const router = express.Router();
+const { auth } = require("../auth/auth");
 
 router.get("/", async (req, res) => {
     res.json({ msg: "Express homepage work" });
 })
 
+router.post("/", auth, async (req, res) => {
+    let validBody = validateGroups(req.body);
+    if (validBody.error) {
+        console.log("not valid body")
+        return res.status(400).json(validBody.error.details)
+    }
+    try {
+        let group = new GroupModel(req.body);
+        group.group_admin = req.tokenData._id;
+        await group.save();
+        res.status(201).json(group);
+
+    }
+    catch (err) {
+        console.log(err);
+        res.status(502).json({ msg: "An error occurred while trying to save the group." })
+    }
+})
 module.exports = router;
